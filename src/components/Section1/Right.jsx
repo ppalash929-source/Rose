5e86d879431fbe6b4a9e19321b9faace46a1f5cb
Rosie❤️ <ppalash929@gmail.com>
2026-06-20 21:26:47 +0530

    committed

--- src/components/Section1/Right.jsx
+++ src/components/Section1/Right.jsx
@@ -2,7 +2,7 @@ import RightPage from "./RightPage"
 const Right = (props) => {
   return (
     <div className="w-full md:w-2/3 flex overflow-x-auto flex-nowrap gap-4 sm:gap-6 md:gap-10 px-2 pb-3
-      [scrollbar-none]
+      
       [-ms-overflow-style:none]
       [&::-webkit-scrollbar]:hidden">
       {props.users.map(function(element){
