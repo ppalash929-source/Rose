c38bc1de3527eb45025690662ff10a561181ff5a
Rosie❤️ <ppalash929@gmail.com>
2026-06-20 21:29:30 +0530

    scroll Changes

--- src/components/Section1/Right.jsx
+++ src/components/Section1/Right.jsx
@@ -3,8 +3,7 @@ const Right = (props) => {
   return (
     <div className="w-full md:w-2/3 flex overflow-x-auto flex-nowrap gap-4 sm:gap-6 md:gap-10 px-2 pb-3
       
-      [-ms-overflow-style:none]
-      [&::-webkit-scrollbar]:hidden">
+      [-ms-overflow-style:none]">
       {props.users.map(function(element){
         return <RightPage key={element.num} img={element.img} tag={element.Tag} num={element.num} para={element.para}/>
       })}
